e9d2973e8a8063a17ad6dc95404ab6b2d603ee05
egypcoder <jackelsafa7@gmail.com>
2025-12-05 06:41:57 +0200

    Update db.ts

--- server/db.ts
+++ server/db.ts
@@ -10,11 +10,13 @@ if (!process.env.DATABASE_URL) {
   );
 }
 
-export const pool = new Pool({ 
+export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
   max: 10,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 10000,
 });
+
 
 export const db = drizzle(pool, { schema });
